37a8740549cd4b9a343f4a9752e6ce0373ef7899
प्रविण सातपुते <pravin.d.s@gmail.com>
2010-09-01 14:58:39 +0530

    added copyright in source files

--- src/engine.h
+++ src/engine.h
@@ -1,4 +1,22 @@
 /* vim:set et sts=4: */
+/* 
+* Copyright (C) 2009-2010 Pravin Satpute <psatpute@redhat.com>
+* Copyright (C) 2009-2010 Red Hat, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 only
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Library General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #ifndef __ENGINE_H__
 #define __ENGINE_H__
 
